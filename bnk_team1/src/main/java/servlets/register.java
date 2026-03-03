d0c22aa470767dc150ef28158a7342b80a4377d5
gy <gunyub1120@gmail.com>
2026-03-03 17:52:42 +0900

    주문등록

---
+++ bnk_team1/src/main/java/servlets/register.java
@@ -0,0 +1,19 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/register")
+public class register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+}
