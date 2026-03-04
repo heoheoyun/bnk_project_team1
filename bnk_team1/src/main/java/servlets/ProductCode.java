448544d5ded51dd5ed2c0023d833cdc6fd19cc39
gg <am0777@naver.com>
2026-03-04 12:36:25 +0900

    [feat] create productCode.jsp and package

--- bnk_team1/src/main/java/servlets/ProductCode.java
+++ bnk_team1/src/main/java/servlets/ProductCode.java
@@ -15,10 +15,4 @@ public class ProductCode extends HttpServlet {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
