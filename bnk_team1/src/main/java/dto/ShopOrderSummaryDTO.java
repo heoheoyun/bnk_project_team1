0317c0bc82b6b1504fdf38ad93a0d4e63cbabd62
hy <hy7780@kakao.com>
2026-03-04 11:36:44 +0900

    점포별 주문목록 java, jsp 생성

---
+++ bnk_team1/src/main/java/dto/ShopOrderSummaryDTO.java
@@ -0,0 +1,44 @@
+package dto;
+
+public class ShopOrderSummaryDTO {
+	private String shopno;
+	private String pcode;
+	private String pname;
+	private int amount;
+
+	public ShopOrderSummaryDTO() {
+		
+	}
+
+	public String getShopno() {
+		return shopno;
+	}
+
+	public void setShopno(String shopno) {
+		this.shopno = shopno;
+	}
+
+	public String getPcode() {
+		return pcode;
+	}
+
+	public void setPcode(String pcode) {
+		this.pcode = pcode;
+	}
+
+	public String getPname() {
+		return pname;
+	}
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+}
